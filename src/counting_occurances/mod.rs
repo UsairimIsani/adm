ae08d58abe48016b8fcfd9a80a9a5c03d03c8150
Usairim Isani <usairim.isani@outlook.com>
2020-07-27 13:56:56 +0500

    Rust Counting Occurances.

--- src/counting_occurances/mod.rs
+++ src/counting_occurances/mod.rs
@@ -1,7 +1,11 @@
 //! # Counting Occurances in Rust via Binary search
-pub fn counting_occurances<T: PartialOrd + Clone + Debug>(col: &[T], key: T) -> Option<usize> {
+type Res = (usize, usize);
+pub fn counting_occurances<T: PartialOrd + Clone + Debug>(col: &[T], key: T) -> Option<Res> {
     let mut result = col.to_vec();
-    counting_occurances_r(&mut result, key, 0, col.len() - 1)
+    Some((
+        counting_occurances_l(&mut result, key.clone(), 0, col.len() - 1).unwrap(),
+        counting_occurances_r(&mut result, key.clone(), 0, col.len() - 1).unwrap(),
+    ))
 }
 fn counting_occurances_r<T: PartialOrd + Clone + Debug>(
     col: &mut [T],
@@ -13,7 +17,7 @@ fn counting_occurances_r<T: PartialOrd + Clone + Debug>(
         "Low : {}, High :{}, Collection : {:?},key:{:?}",
         low, high, col, key
     );
-    if low > high {
+    if low >= high {
         println!("In low>high");
         Some(low)
     } else {
@@ -33,15 +37,44 @@ fn counting_occurances_r<T: PartialOrd + Clone + Debug>(
         }
     }
 }
+fn counting_occurances_l<T: PartialOrd + Clone + Debug>(
+    col: &mut [T],
+    key: T,
+    low: usize,
+    high: usize,
+) -> Option<usize> {
+    println!(
+        "Low : {}, High :{}, Collection : {:?},key:{:?}",
+        low, high, col, key
+    );
+    if low >= high {
+        println!("In low>high");
+        Some(low)
+    } else {
+        println!("In Else");
+        let middle = (low + high) / 2;
+        let val = col[middle].clone();
+        if val < key {
+            println!("In Val > Key");
+            println!(
+                "Low : {}, High :{},val :{:?} ,Collection : {:?}",
+                low, high, val, col
+            );
+            counting_occurances_l(col, key, middle + 1, high)
+        } else {
+            println!("In Val < Key");
+            counting_occurances_l(col, key, low, middle)
+        }
+    }
+}
 use std::fmt::Debug;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_binary_search() {
+    fn test_counting_occurances() {
         use crate::counting_occurances::counting_occurances;
         let arr = vec![3, 5, 5, 5, 6, 7];
-        counting_occurances(&arr, 5);
-        println!("{:?}", arr);
+        println!("{:?}", counting_occurances(&arr, 5).unwrap());
         // assert_eq!(Some(1, 3), binary_search(&arr, 5));
     }
 }
